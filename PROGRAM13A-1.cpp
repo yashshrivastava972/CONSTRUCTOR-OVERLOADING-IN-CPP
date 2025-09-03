84ec5792c8570844d29cd3c40295f4510613826e
Yash Shrivastava <yashshrivastava578@gmail.com>
2025-09-03 09:38:12 +0530

    Update PROGRAM13A-1.cpp

--- PROGRAM13A-1.cpp
+++ PROGRAM13A-1.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+using namespace std;
 
+class Add {
+public:
+
+    Add(int a, int b) {
+        cout << "Sum of two integers: " << (a + b) << endl;
+    }
+
+    Add(float a, float b) {
+        cout << "Sum of two floats: " << (a + b) << endl;
+    }
+
+    Add(int a, int b, int c) {
+        cout << "Sum of three integers: " << (a + b + c) << endl;
+    }
+};
+
+int main() {
+   
+    Add object1(5, 6);          
+    Add object2(5.5f, 6.5f);   
+    Add object3(1, 2, 3);       
+
+    return 0;
+}
+
+// OUTPUT:
+
+// Sum of two integers: 11
+// Sum of two floats: 12
+// Sum of three integers: 6
