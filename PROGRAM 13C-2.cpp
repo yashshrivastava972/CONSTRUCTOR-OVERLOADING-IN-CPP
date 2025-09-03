7944e532adb48d9247a157997dd6fd8dda0470bc
Yash Shrivastava <yashshrivastava578@gmail.com>
2025-09-03 16:12:32 +0530

    Create PROGRAM 13C-2.cpp

---
+++ PROGRAM 13C-2.cpp
@@ -0,0 +1,48 @@
+// NAME: YASH SHRIVASTAVA
+// PRN: 24070123137
+// BATCH: E&TC-B2
+// TITLE: OVERLOAD THE ++ OPERATOR FOR A COUNTER CLASS (EXPERIMENT: 13B-2)
+
+// CODE: 
+
+#include <iostream>
+using namespace std;
+
+class Counter {
+private:
+    int value;
+
+public:
+    Counter(int v = 0) { 
+    
+        value = v;
+    }
+
+  
+    Counter operator++() {
+        ++value;
+        return *this;
+    }
+
+    void show() {
+        cout << "Value: " << value << endl;
+    }
+};
+
+int main() {
+    Counter c1(5);   
+
+    cout << "Before increment: ";
+    c1.show();
+
+    ++c1;  
+    
+    cout << "After increment: ";
+    c1.show();
+
+    return 0;
+}
+// OUTPUT:
+
+// Before increment: Value: 5
+// After increment: Value: 6
